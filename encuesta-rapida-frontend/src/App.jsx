1e39c3e8ddb8b3c7c517b8b17a024734a54757cd
Miguel <bydemon101@gmail.com>
2025-05-30 20:59:02 -0500

    Navbar

--- encuesta-rapida-frontend/src/App.jsx
+++ encuesta-rapida-frontend/src/App.jsx
@@ -2,16 +2,27 @@ import './App.css'
 import CrearEncuesta from './components/CrearEncuesta'
 import VotarEncuesta from './components/VotarEncuesta'
 import ResultadosEncuesta from './components/ResultadosEncuesta'
+import Navbar from './components/Navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function Home() {
+  return (
+    <div className="container text-center mt-5">
+      <h1>Bienvenido a Encuestas Rápidas</h1>
+      <p className="lead">Crea, comparte y vota encuestas de forma sencilla.</p>
+    </div>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<CrearEncuesta />} />
-        <Route path="/votar/:codigo" element={<VotarEncuesta />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/crear" element={<CrearEncuesta />} />
+        <Route path="/encuesta/:codigo" element={<VotarEncuesta />} />
         <Route path="/resultados/:codigo" element={<ResultadosEncuesta />} />
-        {/* Puedes agregar aquí la ruta de resultados si tienes el componente */}
       </Routes>
     </BrowserRouter>
   )
