6b368986d7fc460833ee1ae848ef95a8a95651de
Miguel <bydemon101@gmail.com>
2025-05-30 20:01:25 -0500

    Votar encuesta componente

--- encuesta-rapida-frontend/src/App.jsx
+++ encuesta-rapida-frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import CrearEncuesta from './components/CrearEncuesta'
+import VotarEncuesta from './components/VotarEncuesta'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="app">
-      <CrearEncuesta />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CrearEncuesta />} />
+        <Route path="/votar/:codigo" element={<VotarEncuesta />} />
+        {/* Puedes agregar aquí la ruta de resultados si tienes el componente */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
